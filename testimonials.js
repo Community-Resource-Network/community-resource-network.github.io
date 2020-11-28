0c73b241b1690afcb4796604cd2bfee3de1e81e1
kaedenbrinkman <49798040+kaedenbrinkman@users.noreply.github.com>
2020-11-28 11:58:02 -0800

    Fixed parameter

--- testimonials.js
+++ testimonials.js
@@ -1,32 +1,32 @@
 $.fn.shuffleChildren = function () {
-    $.each(this.get(), function (index, el) {
-      var $el = $(el);
-      var $find = $el.children();
+  $.each(this.get(), function (index, el) {
+    var $el = $(el);
+    var $find = $el.children();
 
-      $find.sort(function () {
-        return 0.5 - Math.random();
-      });
-
-      $el.empty();
-      $find.appendTo($el);
+    $find.sort(function () {
+      return 0.5 - Math.random();
     });
-  };
-  $(document).ready(function () {
-    $(".testimonial-carousel").shuffleChildren();
-    var total = $(".testimonial-carousel")[0].childNodes.length, // get the number of slides
-      rand = Math.floor(Math.random() * total); // random number
-    $(".testimonial-carousel")
-      .slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 10000,
-        autoplay: true,
-        arrows: true,
-        prevArrow: $(".testimonial-carousel-controls .prev"),
-        nextArrow: $(".testimonial-carousel-controls .next"),
-      })
-      .slick("slickGoTo", rand); //navigate to random slide;
-    console.log(rand);
-    //$(".testimonial-carousel").shuffleChildren();
+
+    $el.empty();
+    $find.appendTo($el);
   });
+};
+$(document).ready(function () {
+  $(".testimonial-carousel").shuffleChildren();
+  var total = $(".testimonial-carousel")[0].childNodes.length, // get the number of slides
+    rand = Math.floor(Math.random() * total); // random number
+  $(".testimonial-carousel")
+    .slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      arrows: true,
+      prevArrow: $(".testimonial-carousel-controls .prev"),
+      nextArrow: $(".testimonial-carousel-controls .next"),
+    })
+    .slick("slickGoTo", rand); //navigate to random slide;
+  console.log(rand);
+  //$(".testimonial-carousel").shuffleChildren();
+});
