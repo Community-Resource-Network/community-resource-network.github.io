d7ec039adb37c442f611def01fb714ebdf724f7e
kaedenbrinkman <49798040+kaedenbrinkman@users.noreply.github.com>
2020-11-27 20:24:53 -0800

    Revisions

---
+++ testimonials.js
@@ -0,0 +1,31 @@
+$.fn.shuffleChildren = function () {
+    $.each(this.get(), function (index, el) {
+      var $el = $(el);
+      var $find = $el.children();
+
+      $find.sort(function () {
+        return 0.5 - Math.random();
+      });
+
+      $el.empty();
+      $find.appendTo($el);
+    });
+  };
+  $(document).ready(function () {
+    $(".testimonial-carousel").shuffleChildren();
+    var total = $(".testimonial-carousel")[0].childNodes.length, // get the number of slides
+      rand = Math.floor(Math.random() * total); // random number
+    $(".testimonial-carousel")
+      .slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true,
+        prevArrow: $(".testimonial-carousel-controls .prev"),
+        nextArrow: $(".testimonial-carousel-controls .next"),
+      })
+      .slick("slickGoTo", rand); //navigate to random slide;
+    console.log(rand);
+    //$(".testimonial-carousel").shuffleChildren();
+  });
